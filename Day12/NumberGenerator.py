ad0ef653553534d5b3901edf04b199c755d97029
rohira-dhruv <rohirajdhruv@gmail.com>
2023-02-19 14:47:04 +0530

    add logo

--- Day12/NumberGenerator.py
+++ Day12/NumberGenerator.py
@@ -1,8 +1,10 @@
 import random
+from art import logo
 
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
+print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
